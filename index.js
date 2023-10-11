f25b1acfbd68ebf3aa6b99b2bb5ae15b2a755b78
ChessAITest <147644008+ChessAITest@users.noreply.github.com>
2023-10-11 12:26:19 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+// Searching 
+
+window.addEventListener("load", async (event) => {
+  window.navigator.serviceWorker
+    .register("./sw.js", {
+      scope: __uv$config.prefix,
+    })
+    .then(() => {
+      let url = "www.chess.com";
+      if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
+      else if (!(url.startsWith("https://") || url.startsWith("http://")))
+        url = "http://" + url;
+      sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
+      location.href = "go";
+    });
+});
+  function isUrl(val = "") {
+    if (
+      /^http(s?):\/\//.test(val) ||
+      (val.includes(".") && val.substr(0, 1) !== " ")
+    )
+      return true;
+    return false;
+  }
