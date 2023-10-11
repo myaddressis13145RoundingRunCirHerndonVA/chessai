f752ba0d44fdc17b936c54827eef1c312337f91b
ChessAITest <147644008+ChessAITest@users.noreply.github.com>
2023-10-11 12:35:40 -0400

    Create geography.js

---
+++ h/geography.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/astronomy/',
+    bare: 'https://8vk2d6-6987.csb.app/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/h/algebra.js',
+    bundle: '/h/mathematics.js',
+    config: '/h/geography.js',
+    sw: '/h/english.js',
+};
